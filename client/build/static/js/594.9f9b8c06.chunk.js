e4b2517bebe4948c87c89fc4a750ee04305aa0c1
Pradeep Maurya <pradeep_2101cs59@iitp.ac.in>
2023-12-29 23:29:46 +0530

    added build folder

---
+++ client/build/static/js/594.9f9b8c06.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkclient=self.webpackChunkclient||[]).push([[594],{8594:(e,n,s)=>{s.r(n),s.d(n,{default:()=>a});var r=s(697),l=s(890),c=s(7689),t=s(184);const a=()=>{const e=(0,c.lk)();return console.log(e),(0,t.jsx)(r.Z,{sx:{marginLeft:250},children:(0,t.jsx)(l.Z,{children:"There was an error loading this page."})})}}}]);
+//# sourceMappingURL=594.9f9b8c06.chunk.js.map
